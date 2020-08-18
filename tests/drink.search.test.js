7be8111edadb796ce949f70ed3ff292da748158f
David <drball117@outlook.com>
2020-08-18 02:03:58 -0400

    Added another test for GET /drink route.

--- tests/drink.search.test.js
+++ tests/drink.search.test.js
@@ -24,6 +24,32 @@ describe(`GET /api/drinks?alcoholic=true&category=cocktail&glass=cocktail%20glas
 	});
 });
 
+describe(`GET /api/drinks?alcoholic=true&category=cocktail&glass=cocktail%20glass&ingredient=nutmeg&name=alex`, () => {
+	it(`should return an array of Drink objects`, (done) => {
+		chai
+			.request(server)
+			.get(
+				`/api/drinks?alcoholic=true&category=cocktail&glass=cocktail%20glass&ingredient=nutmeg&name=alex`
+			)
+			.end((err, res) => {
+				expect(err).to.be.null;
+				expect(res).to.have.status(200);
+				expect(res.body)
+					.to.be.an(`Array`)
+					.that.is.not.empty.to.equal([
+						{
+							id: "740858be-529a-476e-9161-1a8dce23bb49",
+							name: "Alexander",
+							image:
+								"https://www.thecocktaildb.com/images/media/drink/urystu1478253039.jpg",
+							ingredients: ["Gin", "Creme de Cacao", "Light cream", "Nutmeg"],
+						},
+					]);
+				done();
+			});
+	});
+});
+
 describe(`GET /api/drinks?alcoholic=null&category=cocktail&glass=cocktail%20glass&ingredient=nutmeg`, () => {
 	it(`should return a 400 bad request error`, (done) => {
 		chai
